ef414415657b52f4e352c35b36339b066854b93a
rickdouglas <rickmiranda23@gmail.com>
2020-02-11 22:10:07 -0300

    Adicionando a lib cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const express = require ('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+
 const app = express();
 
 mongoose.connect('mongodb+srv://rickdouglas:rickdouglas@omnistack9-6p5hm.mongodb.net/omnistack9?retryWrites=true&w=majority',{
     useNewUrlParser :true,
     useUnifiedTopology:true,
 })
-
+app.use(cors)
 app.use(express.json());
 
 app.use(routes);

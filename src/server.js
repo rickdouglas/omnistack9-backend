f315b5df06e3c527aa8b6f02554062bc8cf1e970
rickdouglas <rickmiranda23@gmail.com>
2020-02-19 23:23:30 -0300

    Ajustes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require ('express');
-const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+const routes = require('./routes');
 
 
 const app = express();
@@ -10,9 +11,9 @@ mongoose.connect('mongodb+srv://rickdouglas:rickdouglas@omnistack9-6p5hm.mongodb
     useNewUrlParser :true,
     useUnifiedTopology:true,
 })
-app.use(cors)
+app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
